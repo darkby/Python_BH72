b5e74878ff352cb5951677efecdc512300b62290
Alexander_Bandevich <darkalex.by@gmail.com>
2023-11-13 11:39:16 +0300

    homework

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
-print("Hello world")
+#first task (first way)
+# n1 = (input('Text Please '))
+# n2 = n1.split(' ')
+# print('-'.join(n2))
+#first task (two way)
+# n1 = (input('Text Please '))
+# n2 = (n1.count(' '))
+# print(n1.replace(' ','-', n2))
+#Second task
+# n1 = int(input('Enter first number '))
+# n2 = int(input('Enter two number '))
+# n3 = int(input('Enter tree number '))
+# print(round((n1+n2+n3)/3, 3))
+#Three task
+# name = (input('Enter Name '))
+# age = (input('Enter Age '))
+# city = (input('Enter City '))
+# print('My name %s. I am %s years old. I am from %s'  % (name.capitalize(), age, city.capitalize()))
+# print('My name {name2}. I am {age2} years old. I am from {city2}' .format(name2=name.capitalize(), age2=age, city2=city.capitalize()))
+# print(f'My name {name.capitalize()}. I am {age} years old. I am from {city.capitalize()}')
+#Four task
+# n1 = int(input('First count '))
+# n2 = int(input('Two count '))
+# n3 = int(input('Three count '))
+# b1 = (n1 >= 0) + (n2 >= 0 ) + (n3 >= 0)
+# b2 = (n1 < 0) + (n2 < 0) + (n3 < 0)
+# print('Количество положительных чисел',int(b1))
+# print('Количество отрицательных чисел',int(b2))
 
-a = 1
-b = 2
+#First task (two way)
+
+
+
+
+
+
+
