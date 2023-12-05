2e032bc6b591bebcbbede74c020f63d50a7f3c89
darkby <darkalex.by@gmail.com>
2023-12-05 11:29:16 +0300

    05-12

--- test2.py
+++ test2.py
@@ -1,14 +1,20 @@
-# # #  Дан словарь словарей, ключ внешнего словаря - id пользователя, значение -
-# словарь с данными о пользователе (имя, фамилия, телефон, почта), вывести
-# имена тех, у кого не указана почта (нет ключа email или значение этого ключа -
-# пустая строка)
-
-
-terra = {'blr': ['minsk', 'gomel', 'brest'], 'rus': ['moskow', 'piter', 'kazan'], 'pl': ['warshava', 'bila-podlyaska', 'byalystok']}
-print(terra)
-def find_country(text):
-    for i, j in terra.items():
-        if text in j:
-            print('Country it`s ', i)
-
-find_country(input('Enter city please '))
+# # # #  Дан словарь словарей, ключ внешнего словаря - id пользователя, значение -
+# # словарь с данными о пользователе (имя, фамилия, телефон, почта), вывести
+# # имена тех, у кого не указана почта (нет ключа email или значение этого ключа -
+# # пустая строка)
+#
+#
+# terra = {'100': {'name' : 'alex', 'last_name': 'ivanov', 'phone' : '+375291234567', 'email': 'alex@mail.ru' },
+#          '101': {'name' : 'vova', 'last_name': 'sidoroff', 'phone' : '+375299876541',  },
+#          '102': {'name' : 'sofia', 'last_name': 'sergeeva', 'phone' : '+375296543217', 'email': '' }}
+#
+# def email(dictionary):
+#     for i in dictionary:
+#         if dictionary[i].get('email') == None:
+#             print('Email отсутствует у ID', i, dictionary[i]['name'])
+#             print()
+#         if dictionary[i].get('email') == '':
+#             print('Email не указан у ID', i, dictionary[i]['name'])
+#             print()
+#
+# email(terra)
